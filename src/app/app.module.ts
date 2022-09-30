83b77d13ff8d724a5a7558ce7f61ea91ed14a2c0
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-30 00:25:31 -0500

    feat!: Add Material Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,46 +8,27 @@ import { SwapComponent } from './components/swap/swap.component';
 import { CurrenciesComponent } from './components/currencies/currencies.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
-import { HttpClientModule } from '@angular/common/http';
-
 // Material UI
-import { MatIconModule } from '@angular/material/icon';
-import { CdkMenuModule } from '@angular/cdk/menu';
+import { MaterialModule } from './../material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwapComponent,
     CurrenciesComponent,
-    PortfolioComponent
-  ],
-  imports: [
+    PortfolioComponent,
+   ],
+   imports: [
+    MaterialModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    CdkMenuModule,
     FormsModule,
     ReactiveFormsModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatPaginatorModule,
-    MatInputModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSortModule,
-    MatCardModule,
-    MatTabsModule,
+    MatNativeDateModule,
     HttpClientModule
   ],
   providers: [],
